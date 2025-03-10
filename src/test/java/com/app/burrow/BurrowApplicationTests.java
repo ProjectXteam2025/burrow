f89d40a70811382e5594ad98b9ab01c5d3e2d165
Priyakshee Mahanta <60322729+Priyaksheemahanta@users.noreply.github.com>
2025-03-10 12:20:33 +0530

    Initialize burrow project

---
+++ src/test/java/com/app/burrow/BurrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.burrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
